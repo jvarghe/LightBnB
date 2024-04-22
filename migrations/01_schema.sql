50e35f8b8d52ac1ff4cd9fe9f89aced65137d97f
jvarghe <vcs.inbox.outlook.com>
2024-04-22 17:11:43 -0400

    Re-arrange columns in migrations/01_schema.sql' to group PKs & FKs together.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -41,10 +41,11 @@ CREATE TABLE properties (
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
 );
 
 
@@ -53,6 +54,7 @@ CREATE TABLE property_reviews (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
