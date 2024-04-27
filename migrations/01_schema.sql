a8d4e8669ee418f4de54fd6cf8d34ffd166b8328
jvarghe <vcs.inbox.outlook.com>
2024-04-26 22:06:30 -0400

    Update 'migrations/01_schema.sql' with two new table schemas.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -2,6 +2,8 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
 
@@ -46,6 +48,27 @@ CREATE TABLE reservations (
 
   start_date DATE NOT NULL,
   end_date DATE NOT NULL
+);
+
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+
+  cost_per_night INTEGER NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
+
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
 );
 
 
