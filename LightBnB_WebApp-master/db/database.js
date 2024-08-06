38365663e1e49dfdf9cfce633ce25f4791eb2350
jvarghe <vcs.inbox.outlook.com>
2024-08-06 12:58:45 -0400

    Add code to connect project to database to 'database.js'.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,52 @@
+// JSON "DATABASE"
+// The project started out with these JSON files simulating a database. But
+// they will be replaced with queries to an actual database.
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+
+
+// CONNECTING TO THE POSTGRESQL DATABASE
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb2024"
+});
+
+
+/* TESTING THE CONNECTION TO THE DATABASE
+ *
+ * To test if the database connection is working, execute `npm run local` in
+ * a terminal and de-comment the line below. The terminal should return this
+ * message:
+ *
+ *    Result {
+ *      command: 'SELECT',
+ *      rowCount: 10,
+ *      oid: null,
+ *      rows:
+ *      [ { title: 'Daily every' },
+ *        { title: 'Magic familiar' },
+ *        { title: 'List least' },
+ *        { title: 'Done game' },
+ *        { title: 'Prepare if' },
+ *        { title: 'Roll stranger' },
+ *        { title: 'Rhyme other' },
+ *        { title: 'Remarkable date' },
+ *        { title: 'Student least' },
+ *        { title: 'Political every' } ],
+ *   ...
+ *   }
+ *
+ * The property titles may be different, but you should see 10 titles returned
+ * as required by the query. If you get this response in the terminal, the
+ * connection to the database has been properly established.
+ */
+// pool.query(`SELECT title FROM properties LIMIT 10;`)
+//   .then(response => {console.log(response)});
+
 
 /// Users
 
@@ -8,7 +55,7 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
+const getUserWithEmail = function(email) {
   let resolvedUser = null;
   for (const userId in users) {
     const user = users[userId];
@@ -24,7 +71,7 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
+const getUserWithId = function(id) {
   return Promise.resolve(users[id]);
 };
 
@@ -33,7 +80,7 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
+const addUser = function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -47,7 +94,7 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
+const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
 };
 
@@ -59,7 +106,7 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
+const getAllProperties = function(options, limit = 10) {
   const limitedProperties = {};
   for (let i = 1; i <= limit; i++) {
     limitedProperties[i] = properties[i];
@@ -72,7 +119,7 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
+const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
